0456ab8658161b9a57066c6569ef2f5263678a50
bpoc-mark <markariel.maata@gmail.com>
2021-09-07 14:47:45 +0800

    added section 2 pc and sp view

--- index.php
+++ index.php
@@ -40,6 +40,28 @@
 					<a href="" class="btn_scrolldown"></a>
 				</div>
 			</section>
+			<section class="sect_2">
+				<div class="wrapper">
+					<div class="container">
+						<div class="row">
+							<div class="col">
+								<h3>
+									<span><span>分譲マンションの</span><br><span>管理体制</span>を<span>一新</span>します</span>
+								</h3>
+								<p>皆様がお住まいのマンション共用部や設備は、清潔で安全な管理の下で使用されていますか？<br>
+								現在のマンション管理に少しでもご不満な点があるのであれば、それは管理会社を変更するチャンスです。
+									<img src="/images/top/sp/section_2/img_1.png" alt="">
+								</p>
+								<p>分譲マンション管理 株式会社リアルは、皆様に全ての業務内容をご理解いただけるよう、明確な体制を確立。</p>
+								<p>静岡・浜松から愛知のマンション管理体制を一新すべく経営に力を入れています。</p>
+							</div>
+							<div class="col">
+								<img src="/images/top/section_2/img_1.png" alt="">
+							</div>
+						</div>
+					</div>
+				</div>
+			</section>
 		</main>
 
 		<footer>
